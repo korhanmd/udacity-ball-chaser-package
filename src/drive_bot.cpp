e8ff83958956429ff41d9e8d2b8d94d45cfbb1f1
Korhan Mutludoğan <korhanmd@gmail.com>
2020-12-22 16:44:30 +0300

    Delete the loop

--- src/drive_bot.cpp
+++ src/drive_bot.cpp
@@ -12,15 +12,15 @@ ros::Publisher motor_command_publisher;
 bool handle_drive_request(ball_chaser::DriveToTarget::Request& req, ball_chaser::DriveToTarget::Response& res)
 {
     // TODO: Delete the loop, move the code to the inside of the callback function and make the necessary changes to publish the requested velocities instead of constant values
-    while (ros::ok()) {
-        // Create a motor_command object of type geometry_msgs::Twist
-        geometry_msgs::Twist motor_command;
-        // Set wheel velocities, forward [0.5, 0.0]
-        motor_command.linear.x = 0.5;
-        motor_command.angular.z = 0.0;
-        // Publish angles to drive the robot
-        motor_command_publisher.publish(motor_command);
-    }
+
+    // Create a motor_command object of type geometry_msgs::Twist
+    geometry_msgs::Twist motor_command;
+    // Set wheel velocities, forward [0.5, 0.0]
+    motor_command.linear.x = 0.5;
+    motor_command.angular.z = 0.0;
+    // Publish angles to drive the robot
+    motor_command_publisher.publish(motor_command);
+
     return true;
 }
 
