6225cceb9092cddc7d11dfd39036b62764c53fc0
Korhan Mutludoğan <korhanmd@gmail.com>
2020-12-24 14:25:37 +0300

    Fix assign operator typo

--- src/process_image.cpp
+++ src/process_image.cpp
@@ -30,7 +30,8 @@ void process_image_callback(const sensor_msgs::Image img)
 
 	for (int i = 0; i < img.height * img.step; i += 3){
 		if ((img.data[i] == white_pixel) && (img.data[i+1] == white_pixel) && (img.data[i+2] == white_pixel)){
-			white_location == i;
+			white_location = i;
+			std::cout << "White location: " << white_location << std::endl;
 			break;
 		}
 	}
