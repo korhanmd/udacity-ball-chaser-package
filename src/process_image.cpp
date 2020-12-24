688797d9883062e3d5c033d8cbe558a4dfc791ae
Korhan Mutludoğan <korhanmd@gmail.com>
2020-12-24 11:53:04 +0300

    Check RGB values to detect white pixel

--- src/process_image.cpp
+++ src/process_image.cpp
@@ -20,8 +20,8 @@ void process_image_callback(const sensor_msgs::Image img)
     // Loop through each pixel in the image and check if there's a bright white one
 	int white_location = -1;
 
-	for (int i = 0; i < img.height * img.step; i++){
-		if (img.data[i] == white_pixel){
+	for (int i = 0; i < img.height * img.step; i += 3){
+		if ((img.data[i] == white_pixel) && (img.data[i+1] == white_pixel) && (img.data[i+2] == white_pixel)){
 			white_location == i;
 			break;
 		}
